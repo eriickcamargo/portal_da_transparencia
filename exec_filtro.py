be2677968c6789bc93845b1911d7d644a1f04140
Erick Camargo <ericklimacamargo@gmail.com>
2022-07-29 15:55:29 -0300

    segundo commit global

--- exec_filtro.py
+++ exec_filtro.py
@@ -1,10 +1,11 @@
 from filtros import *
 from filtros import filtro_convenios
+import ezgmail
 
 def sendEmail():
     try:
-        emailBody = f"Bom dia, \nAqui está a relação de convênios sem anexo no Portal da Transparência, foram encontrados no total {len(CONVENIOS_SEM_ANEXO)} convenios sem anexo, até amanhã!"
-        ezgmail.send('ericklimacamargo@gmail.com', 'Relação de Contratos Sem Anexo', emailBody, ['my_excel_test.xlsx'], 'Erick Automate', None,)
+        emailBody = f"Bom dia, \nAqui está a resultado da varredura no Portal da Transparência, até amanhã!"
+        ezgmail.send('ericklimacamargo@gmail.com', 'Pendências do Portal da Transparência', emailBody, ['my_excel_test.xlsx'], 'Erick Automate', None,)
     except Exception as emailError:
         print(f"Houve um erro ao enviar o e-mail: {emailError}")
 
